4b3bc608a8e706b7ac5ce7a20cc50663bbcfa14d
msc25f4 Federico Brancasi (fbrancasi) <msc25f4@badile02.ee.ethz.ch>
2025-05-07 16:59:53 +0200

    Modify FloatConvTemplate and fp32 conv kernel to handle bias

--- TargetLibraries/Generic/src/Convolution_fp32.c
+++ TargetLibraries/Generic/src/Convolution_fp32.c
@@ -33,26 +33,36 @@ void Conv2d_fp32_fp32_fp32_NCHW(const float32_t *__restrict__ pSrcA, uint32_t C,
                                 uint32_t H_padded, uint32_t W_padded,
                                 const float32_t *__restrict__ pSrcB, uint32_t F,
                                 uint32_t P, uint32_t Q, uint32_t SP,
-                                uint32_t SQ, float32_t *__restrict__ pDstC) {
-
+                                uint32_t SQ, float32_t *__restrict__ pDstC,
+                                const float32_t *__restrict__ bias)
+{
   uint32_t H_out = (H_padded - P) / SP + 1;
   uint32_t W_out = (W_padded - Q) / SQ + 1;
 
-  uint32_t c, h, w, f, p, q;
+  for (uint32_t f = 0; f < F; ++f)
+  {
+    for (uint32_t h = 0; h < H_out; ++h)
+    {
+      for (uint32_t w = 0; w < W_out; ++w)
+      {
 
-  for (f = 0; f < F; ++f) {
-    for (h = 0; h < H_out; ++h) {
-      for (w = 0; w < W_out; ++w) {
-        float32_t sum = 0.0f;
-        for (c = 0; c < C; ++c) {
-          for (p = 0; p < P; ++p) {
-            for (q = 0; q < Q; ++q) {
-              sum += pSrcA[c * H_padded * W_padded + (h * SP + p) * W_padded +
-                           (w * SQ + q)] *
+        float32_t sum = (bias != NULL) ? (float32_t)bias[f] : 0.0f;
+
+        for (uint32_t c = 0; c < C; ++c)
+        {
+          for (uint32_t p = 0; p < P; ++p)
+          {
+            const uint32_t a_row =
+                c * H_padded * W_padded + (h * SP + p) * W_padded;
+
+            for (uint32_t q = 0; q < Q; ++q)
+            {
+              sum += pSrcA[a_row + (w * SQ + q)] *
                      pSrcB[f * C * P * Q + c * P * Q + p * Q + q];
             }
           }
         }
+
         pDstC[f * H_out * W_out + h * W_out + w] = sum;
       }
     }
